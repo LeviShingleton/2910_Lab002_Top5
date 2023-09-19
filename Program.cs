d3fbee781135e3dd18f55b0c3d9bb9e3c92aada0
Levi Shingleton <levishing@gmail.com>
2023-09-19 14:57:07 -0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,85 @@
+﻿///////////////////////////////////////////////////////////////////
+// Author: Aaron Shingleton
+// Last Modified: 09-19-2023
+// Purpose: Provided with CSV file "videogames.csv," will print out
+// top 5 global best-sellers per gaming platform.
+/////////////////////////////t//////////////////////////////////////
+namespace _2910_Lab001
+{
+    internal class Program
+    {
+        const string filePath = @"..\..\..\videogames.csv";
+
+        static List<VideoGame> csvGames = new List<VideoGame>();
+
+        static int topQueryDepth = 5;
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                string[] data = File.ReadAllLines(filePath);
+
+                for (int i = 1; i < data.Length - 1; i++)
+                {
+                    string[] attributes = data[i].Split(",");
+                    csvGames.Add(new VideoGame(attributes));
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("The file could not be read:");
+                Console.WriteLine(e.Message);
+                return;
+            }
+
+            // Sort the list entries (rows) by title, ascending
+            csvGames.Sort();
+
+            // Empty dictionary to hold platform-keyed lists of videogames
+            Dictionary<string, List<VideoGame>> byPlatformDict = new();
+            
+            // Populate dictionary using platforms as keys
+            foreach (var group in csvGames.GroupBy(x => x.Platform).OrderBy(x => x.Key))
+            {
+                byPlatformDict.Add(group.Key, group.ToList());
+            }
+
+            // Fetch topQueryDepth number of entries from byPlatformDict keys (platforms); prints out result to console
+            foreach (var platform in byPlatformDict.Keys)
+            {
+                var platformTop = GetTopXInGroup(byPlatformDict, platform, topQueryDepth);
+                Console.WriteLine($"-----The top {platformTop.Count} global best-selling games for {platform}-----");
+                PrintQueryList(platformTop);
+                Console.WriteLine();
+            }
+
+            //PrintQueryList(byPlatformDict["3DO"]);
+        }
+
+        /// <summary>
+        /// Prints all entries of VideoGame list using VideoGame.ToString().
+        /// TODO: Implement method using generic.
+        /// </summary>
+        /// <param name="queryList">The input list to print from.</param>
+        static void PrintQueryList(List<VideoGame> queryList)
+        {
+            foreach (VideoGame game in queryList)
+            {
+                Console.WriteLine(game.ToString());
+            }
+        }
+
+        /// <summary>
+        /// Returns the top takeCount entries in key category of input dictionary<string, List<VideoGame>>.
+        /// </summary>
+        /// <param name="inDict">Reference to dictionary to query</param>
+        /// <param name="key">The column/category to query</param>
+        /// <param name="takeCount">The number of entries to take from the top of the sorted column</param>
+        /// <returns></returns>
+        static List<VideoGame> GetTopXInGroup(Dictionary<string, List<VideoGame>> inDict, string key, int takeCount)
+        {
+            return inDict[key].OrderBy(list => list.Global_Sales).Reverse().Take(takeCount).ToList();
+        }
+    }
+}
---
+++ VideoGame.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2910_Lab001
+{
+    internal class VideoGame : IComparable<VideoGame>
+    {
+        string name;
+        public string Name { get => name; }
+
+        string platform;
+        public string Platform { get => platform; }
+
+        string year;
+        public string Year { get => year; }
+
+        string genre;
+        public string Genre { get => genre; }
+
+        string publisher;
+        public string Publisher { get => publisher; }
+
+        float na_sales;
+        public float NA_Sales { get => na_sales; }
+
+        float eu_sales;
+        public float EU_Sales { get => eu_sales; }
+
+        float jp_sales;
+        public float JP_Sales { get => jp_sales; }
+
+        float other_sales;
+        public float Other_Sales { get => other_sales; }
+
+        float global_sales;
+        public float Global_Sales { get => global_sales; }
+
+        /// <summary>
+        /// Constructor using manual input
+        /// </summary>
+        /// <param name="Name">Game name</param>
+        /// <param name="Platform">Gaming platform(s) that game is supported on</param>
+        /// <param name="Year">Year that game was published</param>
+        /// <param name="Genre">Category of game</param>
+        /// <param name="Publisher">Name of publisher</param>
+        /// <param name="NA_Sales">Proportion of sales in North America</param>
+        /// <param name="EU_Sales">Proportion of sales in European Union</param>
+        /// <param name="JP_Sales">Proportion of sales in Japan</param>
+        /// <param name="Other_Sales">Proportion of sales in other countries</param>
+        /// <param name="Global_Sales"></param>
+        public VideoGame(string Name, string Platform, string Year, string Genre, string Publisher, float NA_Sales, float EU_Sales, float JP_Sales, float Other_Sales, float Global_Sales)
+        {
+            name = Name;
+            platform = Platform;
+            year = Year;
+            genre = Genre;
+            publisher = Publisher;
+            na_sales = NA_Sales;
+            eu_sales = EU_Sales;
+            jp_sales = JP_Sales;
+            other_sales = Other_Sales;
+            global_sales = Global_Sales;
+        }
+
+        /// <summary>
+        /// Constructor using CSV row as input
+        /// </summary>
+        /// <param name="dataArray">Input string array of separated attribute values</param>
+        public VideoGame(string[] dataArray)
+        {
+            name = dataArray[0];
+            platform = dataArray[1];
+            year = dataArray[2];
+            genre = dataArray[3];
+            publisher = dataArray[4];
+            na_sales = Convert.ToSingle(dataArray[5]);
+            eu_sales = Convert.ToSingle(dataArray[6]);
+            jp_sales = Convert.ToSingle(dataArray[7]);
+            other_sales = Convert.ToSingle(dataArray[8]);
+            global_sales = Convert.ToSingle(dataArray[9]);
+        }
+
+        /// <summary>
+        /// Compares a VideoGame object to another VideoGame "other"
+        /// </summary>
+        /// <param name="other"></param>
+        /// <returns>-1, if </returns>
+        /// <exception cref="NotImplementedException"></exception>
+        public int CompareTo(VideoGame? other)
+        {
+            return Name.CompareTo(other.Name);
+        }
+
+        public override string ToString()
+        {
+            return $"{Name}, {Platform}, {Year}, {Genre}, {Publisher}, {NA_Sales}, {EU_Sales}, {JP_Sales}, {Other_Sales}, {Global_Sales}"; 
+        }
+    }
+}
